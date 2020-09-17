496efb3a839c1457b387db96aa35e3510b31aef4
Philipp Mildenberger <philipp.mildenberger@artfabrik.at>
2020-09-17 18:31:20 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Swiper />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Swiper from './components/Swiper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
